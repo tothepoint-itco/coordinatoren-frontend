c8364b33535f86c5d91d17828637833a2b3c41db
Kaj Van der Hallen <kaj.vanderhallen@foresee.be>
2016-04-14 18:24:03 +0200

    Added the front-end for the Contract microservice

--- app/controllers/contract-controller.js
+++ app/controllers/contract-controller.js
@@ -1,13 +1,51 @@
 'use strict';
 
 
-angular.module('coordinatorentoolControllers').controller("ContractController", ["ContractResource", "$uibModal",
-function(Contract, $uibModal) {
+angular.module('coordinatorentoolControllers').controller("ContractController",
+["ContractResource", "BediendeResource", "BusinessUnitResource", "$uibModal",
+function(Contract, Bediende, BusinessUnit, $uibModal) {
     Contract.query(
         (success) => {
             this.contracts = success;
+            this.aggregatedContracts = this.contracts;
+
+            this.aggregatedContracts.map((contract) => {
+
+                Bediende.get(
+                    {id: contract.bediendeId},
+                    (bediende) => {
+                        contract.bediende = bediende;
+                    }
+                );
+
+                BusinessUnit.get(
+                    {id: contract.businessUnitId},
+                    (businessUnit) => {
+                        contract.businessUnit = businessUnit;
+                    }
+                );
+
+            })
         }
     );
+
+    var aggregateContract = (contract) => {
+        Bediende.get(
+            {id: contract.bediendeId},
+            (bediende) => {
+                contract.bediende = bediende;
+            }
+        );
+
+        BusinessUnit.get(
+            {id: contract.businessUnitId},
+            (businessUnit) => {
+                contract.businessUnit = businessUnit;
+            }
+        );
+    }
+
+    this.aggregatedContracts = [];
 
     var createContractModal = {
         templateUrl: "partials/modals/create-contract.html",
@@ -31,12 +69,12 @@ function(Contract, $uibModal) {
 
     this.deleteContract = (contractId) => {
         var index = undefined;
-        var contractToDelete = this.contracts.filter((contract, i) => {
+        var contractToDelete = this.aggregatedContracts.filter((contract, i) => {
             if (contract.id == contractId) { index = i}
             return contract.id == contractId;
         });
 
-        $uibModal.open(this.createConfirmModal("Contract verwijderen", "Ben je zeker dat je het contract tussen "+contractToDelete[0].bediendeId+" en "+contractToDelete[0].businessUnitId+" wil verwijderen?")).result.then(
+        $uibModal.open(this.createConfirmModal("Contract verwijderen", "Ben je zeker dat je het contract tussen "+contractToDelete[0].bediende.voorNaam+" "+contractToDelete[0].bediende.familieNaam+" en "+contractToDelete[0].businessUnit.naam+" wil verwijderen?")).result.then(
             (success) => {
                 Contract.delete(
                     {id: contractToDelete[0].id},
@@ -61,7 +99,9 @@ function(Contract, $uibModal) {
                     contract,
                     (successResult) => {
                         console.log("Contract was saved! Result is %o", successResult);
-                        this.contracts.push(successResult);
+
+                        aggregateContract(successResult);
+                        this.aggregatedContracts.push(successResult);
                     },
                     (errorResult) => {
                         console.log("Saving Contract failed! Result was %o", errorResult);
--- app/controllers/modals/create-contract-controller.js
+++ app/controllers/modals/create-contract-controller.js
@@ -1,12 +1,44 @@
 'use strict';
 
 
-angular.module('coordinatorentoolControllers').controller("CreateContractController", ["$uibModalInstance",
-function($uibModalInstance) {
+angular.module('coordinatorentoolControllers').controller("CreateContractController",
+["$uibModalInstance", "BediendeResource", "BusinessUnitResource",
+function($uibModalInstance, Bediende, BusinessUnit) {
     this.cancel = () => {
         $uibModalInstance.dismiss();
     };
 
+    this.bediendes = [];
+    this.businessUnits = [];
+    this.selectedBediende = undefined;
+    this.selectedBusinessUnit = undefined;
+    this.selectedBediendeLabel = undefined;
+
+    Bediende.query(
+        (bediendes) => {
+            this.bediendes = bediendes;
+        }
+    );
+    BusinessUnit.query(
+        (businessUnit) => {
+            this.businessUnits = businessUnit;
+        }
+    );
+
+    this.selectBediende = (bediende) => {
+        if (bediende != undefined) {
+            this.selectedBediende = bediende;
+            this.selectedBediendeLabel = bediende.voorNaam + " " + bediende.familieNaam;
+            this.contract.bediendeId = bediende.id;
+        }
+    }
+
+    this.selectBusinessUnit = (businessUnit) => {
+        if (businessUnit != undefined) {
+            this.selectedBusinessUnit = businessUnit;
+            this.contract.businessUnitId = businessUnit.id;
+        }
+    }
 
     this.contract = {
         bediendeId: undefined,
