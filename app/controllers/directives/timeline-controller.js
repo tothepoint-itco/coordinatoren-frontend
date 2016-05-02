39967634d3b9ae15b4a58f063d4dee79f27b800c
butrint <butrint.zymberi@hotmail.be>
2016-05-02 18:41:00 +0200

    Added collapse and on klick in frontend timeline

--- app/controllers/directives/timeline-controller.js
+++ app/controllers/directives/timeline-controller.js
@@ -1,8 +1,8 @@
 'use strict';
 
 
-angular.module('coordinatorentoolControllers').controller("TimelineController", ["$rootScope",
-function($rootScope) {
+angular.module('coordinatorentoolControllers').controller("TimelineController", ["$rootScope", "AkkoordResource","BestelbonResource","$uibModal",
+function($rootScope, Akkoord, Bestelbon, $uibModal) {
 
     $rootScope.$watch(
         () => this.data, (newVal, oldVal) => {
@@ -13,6 +13,31 @@ function($rootScope) {
         true
     );
 
+    this.akkoordModal = (title, body) => {
+        return {
+            templateUrl: "partials/modals/akkoord.html",
+            controller: "AkkoordModalController",
+            controllerAs: "gmCtrl",
+            keyboard: false,
+            resolve: {
+                title: () => title,
+                body: () => body
+            }
+        }
+    }
+    this.bestelbonModal = (title, body) => {
+        return {
+            templateUrl: "partials/modals/bestelbon.html",
+            controller: "BestelbonModalController",
+            controllerAs: "gmCtrl",
+            keyboard: false,
+            resolve: {
+                title: () => title,
+                body: () => body
+            }
+        }
+    }
+
 
     this.drawTimeline = (data) => {
         var container = document.getElementById("visualization-"+data.consultant.id);
@@ -21,10 +46,57 @@ function($rootScope) {
 
         var groups = [];
 
+        document.getElementById(data.consultant.id).onclick =  (event) => {
+        var props = timeline.getEventProperties(event)
+        this.timeLineId= props.item
+
+        if(this.timeLineId != undefined && this.timeLineId.substring(0,1)=='A'){
+            console.log(this.timeLineId.substring(1))
+            Akkoord.get(
+                {id:this.timeLineId.substring(1)},
+                (success) =>{
+                    console.log("Akkoord get method %o",success)
+                    $uibModal.open(this.akkoordModal("Akkoord met project code : "+success.projectCode, success)).result.then(
+                        (success) => {
+                        console.log("JA!");
+                        },
+                        (error) => {
+                            console.log("Nee!");
+                        }
+                    )
+                }
+            );
+
+        }
+        else if(this.timeLineId != undefined && this.timeLineId.substring(0,1)=='B'){
+            Bestelbon.get(
+                {id:this.timeLineId.substring(1)},
+                (success) =>{
+                    console.log("Bestelbon get method %o",success)
+                    $uibModal.open(this.bestelbonModal("Bestelbon met project code : "+success.projectCode, success)).result.then(
+                        (success) => {
+                        console.log("JA!");
+                        },
+                        (error) => {
+                            console.log("Nee!");
+                        }
+                    )
+                }
+            );
+            console.log("DIEDE")
+
+        }
+        else{
+            console.log("Nothing selected!")
+        }
+        //console.log(props);
+        //console.log(this.foo);
+        }
+
         data.akkoorden.map((akkoordAggregated) => {
             groups.push({id: akkoordAggregated.opdracht.id, content: akkoordAggregated.opdracht.klant});
-
             dataSetArray.push({
+                id: "A"+akkoordAggregated.akkoord.id,
                 group: akkoordAggregated.opdracht.id,
                 //content: akkoordAggregated.opdracht.klant,
                 start: akkoordAggregated.akkoord.informeelStartDatum,
@@ -35,6 +107,7 @@ function($rootScope) {
 
             akkoordAggregated.bestelbonnen.map((bestelbon) => {
                 var entry = {
+                    id: "B"+bestelbon.id,
                     group: akkoordAggregated.opdracht.id,
                     //content: akkoordAggregated.opdracht.klant,
                     start: bestelbon.startDatum,
@@ -55,10 +128,13 @@ function($rootScope) {
             stack: false,
             selectable: false,
             locale: 'nl'
+
         };
 
         var timeline = new vis.Timeline(container, items, groups, options);
     }
+
+
 
 
 }]);
--- app/controllers/planning-controller.js
+++ app/controllers/planning-controller.js
@@ -6,7 +6,28 @@ function(BusinessUnit, Contract, ConsultantAggregated) {
 
     ConsultantAggregated.query(
         (success) => {
+            this.contract = {};
             this.aggregatedConsultants = success;
+            console.log("aggregatedConsultants %o",success);
+            this.aggregatedConsultants.map((aggregatedConsultant)=>
+                Contract.query(
+                    {consultantId: aggregatedConsultant.consultant.id},
+                    (contract)=> {
+                        console.log("contract %o", contract[0].startDatum)
+                        this.aggregatedConsultant.startDatum = contract[0].startDatum;
+                        this.aggregatedConsultant.eindDatum  = contract[0].eindDatum;
+                        BusinessUnit.query(
+                            {id:this.contract.businessUnitId},
+                            (businessUnit) =>{
+                                this.contract.businessUnit = businessUnit[0].naam;
+                                console.log("contract %o",this.contract.businessUnit)
+                            }
+                        )
+                    }
+                )
+
+            )
+
         },
         (error) => {
             console.log("Call failed, result was %o", error);
