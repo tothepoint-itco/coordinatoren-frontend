062347abb0603244c21c80ce930ce8f4d9a7e2fa
Kaj Van der Hallen <kaj.vanderhallen@foresee.be>
2016-06-08 10:00:22 +0200

    Dateformat within tooltips fixed (planning overview)

--- app/controllers/directives/timeline-overview-controller.ts
+++ app/controllers/directives/timeline-overview-controller.ts
@@ -62,7 +62,7 @@ function($rootScope, Akkoord, Bestelbon, $uibModal) {
                     start: moment(akkoordAggregated.akkoord.informeelStartDatum, "DD/MM/YYYY"),
                     end: moment(akkoordAggregated.akkoord.informeelEindDatum, "DD/MM/YYYY"),
                     style: "background-color: #d5ddf7",
-                    title: "Akkoord voor: "+ akkoordAggregated.opdracht.klant +"\nLoopt informeel van: " + moment(akkoordAggregated.akkoord.informeelStartDatum).format("D MMM") + " tot " + moment(akkoordAggregated.akkoord.informeelEindDatum).format("D MMM")
+                    title: "Akkoord voor: "+ akkoordAggregated.opdracht.klant +"\nLoopt informeel van: " + moment(akkoordAggregated.akkoord.informeelStartDatum, "DD/MM/YYYY").format("D MMM") + " tot " + moment(akkoordAggregated.akkoord.informeelEindDatum, "DD/MM/YYYY").format("D MMM")
                 });
 
                 akkoordAggregated.bestelbonnen.map((bestelbon) => {
@@ -73,7 +73,7 @@ function($rootScope, Akkoord, Bestelbon, $uibModal) {
                         end: moment(bestelbon.eindDatum, "DD/MM/YYYY"),
                         content: "<p></p>",
                         style: "background-color: #20bd00; opacity: 0.3",
-                        title: "Bestelbon voor: "+ akkoordAggregated.opdracht.klant +"\nLoopt van: " + moment(bestelbon.startDatum).format("D MMM") + " tot " + moment(bestelbon.eindDatum).format("D MMM")
+                        title: "Bestelbon voor: "+ akkoordAggregated.opdracht.klant +"\nLoopt van: " + moment(bestelbon.startDatum, "DD/MM/YYYY").format("D MMM") + " tot " + moment(bestelbon.eindDatum, "DD/MM/YYYY").format("D MMM")
                     });
                 });
 
