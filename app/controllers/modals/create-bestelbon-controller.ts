693ca12fb5049dc0d6ffb72cb229a37105139a78
butrint <butrint.zymberi@hotmail.be>
2016-06-20 23:33:12 +0200

    Changed create bestelbon modal and added placeholders to date in every create modal

--- app/controllers/modals/create-bestelbon-controller.ts
+++ app/controllers/modals/create-bestelbon-controller.ts
@@ -1,7 +1,10 @@
 'use strict'
 
-angular.module('coordinatorentoolControllers').controller("CreateBestelbonController",['$uibModalInstance', "updateMode", "bestelbon",
-function($uibModalInstance, updateMode, bestelbon){
+angular.module('coordinatorentoolControllers').controller("CreateBestelbonController",['$uibModalInstance', "updateMode", "bestelbon","AkkoordResource",
+function($uibModalInstance, updateMode, bestelbon, Akkoord){
+    this.akkoorden = [];
+    this.selectedAkkoord = undefined;
+    this.selectedAkkoordLabel = undefined;
     if ((bestelbon != null || bestelbon != undefined) && updateMode == true) {
         this.bestelbon = bestelbon;
     } else {
@@ -10,6 +13,26 @@ function($uibModalInstance, updateMode, bestelbon){
             startDatum: undefined,
             eindDatum: undefined
         };
+    }
+    Akkoord.query(
+        (akkoorden) => {
+            this.akkoorden = akkoorden;
+            if (this.updateMode == true && this.bestelbon.akkoordId != undefined) {
+                akkoorden.map((akkoord) => {
+                    if (this.bestelbon.akkoordId == akkoord.id) {
+                        this.selectAkkoord(akkoord);
+                    }
+                });
+            }
+        }
+    );
+    this.selectAkkoord = (akkoord) => {
+        if (akkoord != undefined) {
+            this.selectedAkkoord = akkoord;
+            this.selectedAkkoordLabel = akkoord.projectCode;
+            this.bestelbon.akkoordId = akkoord.id;
+            console.log(this.selectedAkkoordLabel)
+        }
     }
 
     this.updateMode = updateMode;
