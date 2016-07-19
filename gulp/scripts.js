fe75753efe44abd55879fad243974f702bcb5c4b
Lars van Herk <lars.vanherk@tothepoint.company>
2016-07-19 22:03:23 +0200

    Separated gulpfile into multiple, easier to maintain module files. Added SASS support and modified HTML to use app and vendor css files.

---
+++ gulp/scripts.js
@@ -0,0 +1,28 @@
+var gulp = require('gulp');
+var uglify = require('gulp-uglify');
+
+var ts = require('gulp-typescript');
+ts.reporter.nullReporter()
+var tsProject = ts.createProject('tsconfig.json');
+
+/*
+ *
+ *          GULP TASKS
+ * ## TYPESCRIPT & COMPRESSION ##
+ *
+ */
+
+gulp.task('tscompile', function() {
+    tsProject.src()
+    .pipe(ts(tsProject, undefined, ts.reporter.nullReporter()))
+    .js
+    .pipe(gulp.dest('.'));
+});
+
+gulp.task('uglify', function() {
+    if (environments.production()) {
+        gulp.src('app/*.js')
+          .pipe(uglify())
+          .pipe(gulp.dest('app'));
+    }
+});
